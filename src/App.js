38f5718b7c38b82223e277d41a35b7e1bdef0871
Himanshu Raj <himanshu211raj@gmail.com>
2021-12-02 13:39:10 +0530

    video 13 done

--- src/App.js
+++ src/App.js
@@ -2,19 +2,34 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
+import Alert from "./components/Alert";
 
 // import About from "./components/About";
 
 function App() {
   const [mode, setMode] = useState("light"); // whether darkmode enabled or not
 
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500)
+  }
+
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode has been Enabled", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("light Mode has been Enabled", "success");
     }
   };
   return (
@@ -22,8 +37,13 @@ function App() {
       {/* <Navbar title="TextUtils" about="About TextUtils"/>
       <Navbar /> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" mode={mode} />
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyze below"
+          mode={mode}
+        />
         {/* <About /> */}
       </div>
     </>
